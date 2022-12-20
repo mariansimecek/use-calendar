05f3272b1f03615b36fa4fc97c3ed11320372caf
Marian Šimeček <mariansimecek@gmail.com>
2022-12-20 22:07:06 +0100

    Semi functional state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import { format } from "date-fns";
+import { cs } from "date-fns/locale";
 import { useCalendar } from "./libary/use-calendar";
 
 function App() {
-  const { prev, next, items, mode, weeks, options, zoomIn, zoomOut, currentDate } = useCalendar({});
+  const { prev, next, items, mode, weeks, options, zoomIn, zoomOut, currentDate } = useCalendar({ alignByWeek: true, appendDaysToFillRect: true });
   return (
     <div>
-      <div>
-        <button onClick={() => { prev() }}>prev</button>
-        <button onClick={() => { zoomOut() }}>{JSON.stringify(currentDate)}</button>
-        <button onClick={() => { next() }}>next</button>
-        <div>
+      <div className="w-96">
+        <div className="flex justify-between ">
+          <button className="btn py-4 px-6 bg-blue-400" onClick={() => { prev() }}>prev</button>
+          <button onClick={() => { zoomOut() }}>{JSON.stringify(currentDate)}</button>
+          <button className="btn py-4 px-6 bg-blue-400" onClick={() => { next() }}>next</button>
+        </div>
+        <div className="grid grid-cols-7">
           {options?.alignByWeek && weeks.map((week) => (
-            <div>{week}</div>
+            <div className={`aspect-square flex items-center justify-center `}>{week}</div>
           ))}
           {items.map((date) => (
-            <div onClick={() => { zoomIn() }}>{date.num}</div>
+            <div className={`aspect-square ${date.inMonth ? 'bg-zinc-300' : 'bg-zinc-100'} flex flex-col items-center justify-center hover:bg-zinc-300 cursor-pointer`} onClick={() => { zoomIn(date.date) }}>
+                <span className="font-bold">{format(date.date, 'dd', { locale: cs })}</span>
+                <span className="text-xs">{format(date.date, 'E', { locale: cs })}</span>
+            </div>
           ))}
         </div>
       </div>
--- src/libary/use-calendar.tsx
+++ src/libary/use-calendar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 //TODO: hour mode
 //TODO: add useCallback and useMemo to optimize
 
@@ -19,6 +19,7 @@ type CalendarMode = "month" | "year" | "decade"
 
 interface DateUnit {
   num: number,
+  type: "day" | "month" | "year"
   date: Date
   inMonth?: boolean
 }
@@ -29,28 +30,35 @@ export const useCalendar = (options: UseCalendarOptions) => {
   const [mode, setMode] = useState<CalendarMode>(options?.initalMode || "month")
   const yearToRender = options?.yearToRender || 14
 
-  function moveDateByDay(date: Date, amount: number, direction: boolean): Date { }
-  function moveDateByMonth(date: Date, amount: number, direction: boolean): Date { }
-  function moveDateByYear(date: Date, amount: number, direction: boolean): Date { }
+  function moveDateByDay(date: Date, amount: number, direction: boolean): Date {
+    return new Date(date.setDate(date.getDate() + (direction ? amount : -amount)))
+  }
+  function moveDateByMonth(date: Date, amount: number, direction: boolean): Date {
+    return new Date(date.setMonth(date.getMonth() + (direction ? amount : -amount)))
+  }
+  function moveDateByYear(date: Date, amount: number, direction: boolean): Date {
+    return new Date(date.setFullYear(date.getFullYear() + (direction ? amount : -amount)))
+  }
 
   function prev() {
     switch (mode) {
-      case "month": setCurrentDate(moveDateByDay(currentDate, 1, false)); break;
-      case "year": setCurrentDate(moveDateByMonth(currentDate, 12, false)); break;
+      case "month": setCurrentDate(moveDateByMonth(currentDate, 1, false)); break;
+      case "year": setCurrentDate(moveDateByYear(currentDate, 1, false)); break;
       case "decade": setCurrentDate(moveDateByYear(currentDate, yearToRender, false)); break;
-      default: console.warn("mode not found"); break;
+      default: console.warn("Mode not found"); break;
     }
   }
   function next() {
     switch (mode) {
-      case "month": setCurrentDate(moveDateByDay(currentDate, 1, true)); break;
-      case "year": setCurrentDate(moveDateByMonth(currentDate, 12, true)); break;
+      case "month": setCurrentDate(moveDateByMonth(currentDate, 1, true)); break;
+      case "year": setCurrentDate(moveDateByYear(currentDate, 1, true)); break;
       case "decade": setCurrentDate(moveDateByYear(currentDate, yearToRender, true)); break;
-      default: console.warn("mode not found"); break;
+      default: console.warn("Mode not found"); break;
     }
   }
 
-  function zoomIn() {
+  function zoomIn(date: Date) {
+    setCurrentDate(date)
     switch (mode) {
       case "year": setMode("month"); break;
       case "decade": setMode("year"); break;
@@ -70,29 +78,90 @@ export const useCalendar = (options: UseCalendarOptions) => {
 
   function getItemsToRender() {
     switch (mode) {
-      case "month": return getMonthItems()
-      case "year": return getYearItems()
-      case "decade": return getDecadeItems()
+      case "month": return getMonthItems(currentDate)
+      case "year": return getYearItems(currentDate)
+      case "decade": return getDecadeItems(currentDate)
       default: return []
     }
   }
-  function getMonthItems() {
-    // generate month items
-    // prepend if needed
-    // append to fill rect if needed
+  function getMonthItems(currentDate: Date): DateUnit[] {
 
-    throw new Error("Function not implemented.")
+    const monthLength = getDaysInMonth(currentDate);
+    const days: DateUnit[] = []
+    for (let index = 0; index < monthLength; index++) {
+      days.push({
+        num: index + 1,
+        type: "day",
+        date: dateContructor(currentDate, { day: index + 1 }),
+        inMonth: true
+      })
+    }
+    if (options.alignByWeek) {
+      const firstDay = days[0].date.getDay()
+      const missingDaysCount = firstDay - (options.weekStartsOn || 0)
+
+      const daysToPrepend: DateUnit[] = []
+      let prependigDay = currentDate
+      for (let index = 0; index < missingDaysCount; index++) {
+        prependigDay = moveDateByDay(prependigDay, 1, false)
+        daysToPrepend.push({
+          type: "day",
+          date: prependigDay,
+          inMonth: false,
+          num: prependigDay.getDate()
+        })
+        days.unshift(...daysToPrepend)
+      }
+    }
+    if (options.appendDaysToFillRect) {
+      const fillCount = Math.ceil((days.length / 7) * 7 - days.length)
+      console.log(fillCount)
+      const daysToAppend: DateUnit[] = []
+      let appendingDay = currentDate
+      for (let index = 0; index < fillCount; index++) {
+        appendingDay = moveDateByDay(appendingDay, 1, true)
+        daysToAppend.push({
+          type: "day",
+          date: appendingDay,
+          inMonth: false,
+          num: appendingDay.getDate()
+        })
+      }
+      days.push(...daysToAppend)
+    }
+    return days
   }
 
-  function getYearItems() {
-    throw new Error("Function not implemented.")
+  function dateContructor(date: Date, { year, month, day }: { year?: number, month?: number, day?: number }) {
+    return new Date(year || date.getFullYear(), month || date.getMonth(), day || date.getDate())
+  }
+  function getYearItems(currentDate: Date): DateUnit[] {
+    const months: DateUnit[] = []
+    for (let index = 0; index < 12; index++) {
+      months.push({
+        num: index + 1,
+        type: "month",
+        date: dateContructor(currentDate, { month: index })
+      })
+    }
+    console.log(months)
+    return months
   }
 
-  function getDecadeItems() {
-    throw new Error("Function not implemented.")
+  function getDecadeItems(currentDate: Date): DateUnit[] {
+    const year = currentDate.getFullYear()
+    const years: DateUnit[] = []
+    for (let index = 0; index < yearToRender; index++) {
+      years.push({
+        num: year + index,
+        type: "year",
+        date: dateContructor(currentDate, { year: year + index })
+      })
+    }
+    return years
   }
 
-  const items:DateUnit[] = getItemsToRender()
+  const items: DateUnit[] = getItemsToRender()
 
   function getWeeks() {
     // get weeks and shift by weekStartsOn
@@ -105,4 +174,8 @@ export const useCalendar = (options: UseCalendarOptions) => {
 
 
 
+
+function getDaysInMonth(date: Date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+}
 
